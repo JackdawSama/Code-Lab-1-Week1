8a01b0746f7f5ed34e129da8018b4d9c3e8b86c8
Akshay Bharadhwaj <akshaybharadhwaj98@gmail.com>
2022-01-30 04:27:15 -0500

    Updated CactusSpawner

--- Dino/Assets/Scripts/CactusSpawner.cs
+++ Dino/Assets/Scripts/CactusSpawner.cs
@@ -4,15 +4,19 @@ using UnityEngine;
 
 public class CactusSpawner : MonoBehaviour
 {
+    public GameObject cactus;
+    public float spawnTimer;
+    public float spawnDelay;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        InvokeRepeating("Spawner", spawnTimer, spawnDelay);                     //invokes the function spawner at timed intervals
     }
 
-    // Update is called once per frame
-    void Update()
+    public void Spawner()
     {
-        
+        spawnDelay = Random.Range(0.5f, 1.0f);                                  //randomises the delay inbetween spawns
+        Instantiate(cactus, transform.position, transform.rotation);           //creates an object at the said transform
     }
 }
